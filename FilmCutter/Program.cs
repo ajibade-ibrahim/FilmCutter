3c4eb72843efbe1892b374bdc379364b2ea0ffde
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-01-22 05:14:03 +0100

    Formatted file

--- FilmCutter/Program.cs
+++ FilmCutter/Program.cs
@@ -1,50 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using MediaToolkit;
 using MediaToolkit.Model;
 using MediaToolkit.Options;
-using System;
-using System.IO;
-using System.Linq;
-
 
 namespace FilmCutter
 {
     public class Program
     {
-        static void Main()
-        {
-            const string videofilename = @"D:\Matches\Other\The Unmissable Match - Jay-Jay Okocha vs Carles Puyol.MP4";
-            const string periodsFilename = @"D:\text files\Jay Jay vs Team Puyol.txt";
-
-            var periods = GetPeriodsFromFile(periodsFilename);
-
-            foreach (var period in periods)
-            {
-                Console.WriteLine("Abt to cut period {0}", period);
-                CutFile(videofilename, period, periods.ToList().IndexOf(period));
-            }
-
-            Console.WriteLine("Finished Cutting Periods");
-            Console.ReadLine();
-
-        }
-
         private static void CutFile(string filename, string period, int index)
         {
             try
             {
                 //period: 00:12:23 - 01:45:45
-                var inputFile = new MediaFile { Filename = filename };
-                var outputname = filename.Split(new[] { ".mp4" }, StringSplitOptions.None)[0];
-                var outputFile = new MediaFile { Filename = string.Format("{0}{1}{2}", outputname, "~" + (index + 1), ".mp4") };
+                var inputFile = new MediaFile
+                {
+                    Filename = filename
+                };
+
+                var outputname = filename.Split(
+                    new[]
+                    {
+                        ".mp4"
+                    },
+                    StringSplitOptions.None)[0];
+
+                var outputFile = new MediaFile
+                {
+                    Filename = string.Format("{0}{1}{2}", outputname, "~" + (index + 1), ".mp4")
+                };
+
                 var t1 = period.Split('-')[0];
                 Console.WriteLine("Time t1 : {0}", t1);
 
                 var t2 = period.Split('-')[1];
                 Console.WriteLine("Time t2 : {0}", t2);
 
-                TimeSpan span1 = GetTimeSpan(t1.Trim());
-                TimeSpan span2 = GetTimeSpan(t2.Trim());
+                var span1 = GetTimeSpan(t1.Trim());
+                var span2 = GetTimeSpan(t2.Trim());
 
                 using (var engine = new Engine())
                 {
@@ -60,11 +55,10 @@ namespace FilmCutter
                     options.CutMedia(span1, span3);
                     options.VideoSize = VideoSize.Hd720;
                     options.VideoAspectRatio = VideoAspectRatio.R16_9;
-                    engine.CustomCommand("-deinterlace"); 
+                    engine.CustomCommand("-deinterlace");
                     Console.WriteLine("Cutting and converting period {0}", period);
                     engine.Convert(inputFile, outputFile, options);
                     Console.WriteLine("Done cutting period {0}", period);
-
                 }
             }
             catch (IndexOutOfRangeException ind)
@@ -77,26 +71,10 @@ namespace FilmCutter
             }
         }
 
-        private static TimeSpan GetTimeSpan(string t1)
-        {
-            var x = t1.Split(':');
-            var s = x.Reverse();
-            var enumerable = s as IList<string> ?? s.ToList();
-            string seconds = enumerable.ElementAtOrDefault(0);
-            int sec = GetNumber(seconds);
-            string minutes = enumerable.ElementAtOrDefault(1);
-            int min = GetNumber(minutes);
-            string hour = enumerable.ElementAtOrDefault(2);
-            int hr = GetNumber(hour);
-
-
-            var time = new TimeSpan(hr, min, sec);
-            return time;
-        }
-
         private static int GetNumber(string seconds)
         {
-            int sec = 0;
+            var sec = 0;
+
             if (!string.IsNullOrEmpty(seconds))
             {
                 try
@@ -126,6 +104,39 @@ namespace FilmCutter
             }
 
             return periods;
+        }
+
+        private static TimeSpan GetTimeSpan(string t1)
+        {
+            var x = t1.Split(':');
+            var s = x.Reverse();
+            var enumerable = s as IList<string> ?? s.ToList();
+            var seconds = enumerable.ElementAtOrDefault(0);
+            var sec = GetNumber(seconds);
+            var minutes = enumerable.ElementAtOrDefault(1);
+            var min = GetNumber(minutes);
+            var hour = enumerable.ElementAtOrDefault(2);
+            var hr = GetNumber(hour);
+
+            var time = new TimeSpan(hr, min, sec);
+            return time;
+        }
+
+        private static void Main()
+        {
+            const string videofilename = @"D:\Matches\Other\The Unmissable Match - Jay-Jay Okocha vs Carles Puyol.MP4";
+            const string periodsFilename = @"D:\text files\Jay Jay vs Team Puyol.txt";
+
+            var periods = GetPeriodsFromFile(periodsFilename);
+
+            foreach (var period in periods)
+            {
+                Console.WriteLine("Abt to cut period {0}", period);
+                CutFile(videofilename, period, periods.ToList().IndexOf(period));
+            }
+
+            Console.WriteLine("Finished Cutting Periods");
+            Console.ReadLine();
         }
     }
 }
